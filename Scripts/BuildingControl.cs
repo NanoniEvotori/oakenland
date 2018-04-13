85b196ca5ba238ef906d4e7e5702e37c5108fad3
NanoniEvotori <38356516+NanoniEvotori@users.noreply.github.com>
2018-04-13 18:33:49 +0300

    Add files via upload

---
+++ Scripts/BuildingControl.cs
@@ -0,0 +1,329 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+namespace Com.RegalStick.Oakenland{
+	public class BuildingControl : MonoBehaviour, IPunObservable {
+		#region private variables
+		private RaycastHit hit;
+		private Camera cam;
+		private GameObject unitPref;
+		private bool isSetBelow;
+		#endregion
+		#region public variables
+		public bool isActive;
+		public bool isSet;
+		public string buildingId;
+		public string userId;
+		public int level;
+		public int health;
+		public int maxSpace;
+		public int currentSpace=0;
+		public float timeToBuild;
+		public float timeTillFinish;
+		public enum buildingType
+		{
+			camp, storage, farm, lumbermill, baracks
+		}
+		public buildingType type;
+		public GameObject tileBelow;
+		public List <Vector3> spawnPoints;
+		public List <GameObject> units;
+		public Text textUnits;
+		public Text textLevel;
+		#endregion
+		void Awake()
+		{
+			unitPref = (GameObject)Resources.Load ("pref_unit");
+			cam = Camera.main;
+		}
+		void Start()
+		{
+			//textUnits.text = "Units: " + units.Count;
+		}
+		void Update()
+		{
+			if(GetComponent<PhotonView>().isMine == false && PhotonNetwork.connected == true)
+			{
+				return;
+			}		
+			if (isSet == true) 
+			{
+				textUnits.text = "Units: " + units.Count.ToString ();
+			}
+		}
+		#region private methods
+		void CheckRaycast()
+		{
+			if (Physics.Raycast (gameObject.transform.position,Vector3.down, out hit,1.0f)) 
+			{
+				if (hit.transform.name.Contains ("Tile")) 
+				{
+					tileBelow = null;
+				}
+			}
+		}
+		#endregion
+		#region public methods
+		/// <summary>
+		/// Set parameters for a camp.
+		/// </summary>
+		/// <param name="userId">User identification ID.</param>
+		/// <param name="level">Building level.</param>
+		public void SetToCamp(string userId, int level)
+		{//TODO Camp building comfirmation notification
+			this.userId = userId;
+			buildingId = FindObjectOfType<Generator> ().GenerateId ();
+			type = buildingType.camp;
+			this.level = level;
+			if (level == 0) 
+			{
+				health = 20;
+				maxSpace = 5;
+				timeToBuild = 0;
+			}
+			else if (level == 1) 
+			{
+				health = 120;
+				maxSpace = 10;
+				timeToBuild = 10;
+			}
+			else if (level == 2)
+			{
+				health = 200;
+				maxSpace = 14;
+				timeToBuild = 20;
+			}
+			else if(level == 3)
+			{
+				health = 250;
+				maxSpace = 17;
+				timeToBuild = 30;
+			}
+			isSet = true;
+		}
+		/// <summary>
+		/// Sets parameters for a storage.
+		/// </summary>
+		/// <param name="userId">User identification ID.</param>
+		/// <param name="level">Storage level.</param>
+		public void SetToStorage(string userId, int level)
+		{
+			this.userId = userId;
+			buildingId = FindObjectOfType<Generator> ().GenerateId ();
+			type = buildingType.storage;
+			this.level = level;
+			if (level == 0) 
+			{
+				health = 20;
+				maxSpace = 0;
+				timeToBuild = 0;
+			}
+			else if (level == 1) 
+			{
+				health = 60;
+				maxSpace = 200;
+				timeToBuild = 10;
+			}
+			else if(level == 2)
+			{
+				health = 70;
+				maxSpace = 300;
+				timeToBuild = 20;
+			}
+			else if(level == 3)
+			{
+				health = 80;
+				maxSpace = 400;
+				timeToBuild = 30;
+			}
+			isSet = true;
+		}
+		/// <summary>
+		/// Sets parameters for a farm.
+		/// </summary>
+		/// <param name="userId">User identification ID.</param>
+		/// <param name="level">Farm level.</param>
+		public void SetToFarm(string userId, int level)
+		{
+			this.userId = userId;
+			buildingId = FindObjectOfType<Generator> ().GenerateId ();
+			type = buildingType.farm;
+			this.level = level;
+			if (level == 0) 
+			{
+				health = 10;
+				maxSpace = 0;
+				timeToBuild = 0;
+			}
+			else if (level == 1) 
+			{
+				health = 40;
+				maxSpace = 1;
+				timeToBuild = 10;
+			}
+			else if(level == 2)
+			{
+				health = 40;
+				maxSpace = 2;
+				timeToBuild = 20;
+			}
+			else if(level == 3)
+			{
+				health = 40;
+				maxSpace = 3;
+				timeToBuild = 30;
+			}
+			isSet = true;
+		}
+		/// <summary>
+		/// Sets parameters for a Lumbermill.
+		/// </summary>
+		/// <param name="userId">User identification ID.</param>
+		/// <param name="level">Lumbermill level.</param>
+		public void SetToLumbermill(string userId, int level)
+		{
+			this.userId = userId;
+			buildingId = FindObjectOfType<Generator> ().GenerateId ();
+			type = buildingType.lumbermill;
+			this.level = level;
+			if (level == 0) 
+			{
+				health = 10;
+				maxSpace = 0;
+				timeToBuild = 0;
+			}
+			else if (level == 1) 
+			{
+				health = 40;
+				maxSpace = 1;
+				timeToBuild = 10;
+			}
+			else if(level == 2)
+			{
+				health = 40;
+				maxSpace = 2;
+				timeToBuild = 20;
+			}
+			else if(level == 3)
+			{
+				health = 40;
+				maxSpace = 3;
+				timeToBuild = 30;
+			}
+			isSet = true;
+		}
+		/// <summary>
+		/// Sets parameters for a baracks.
+		/// </summary>
+		/// <param name="userId">User identification ID.</param>
+		/// <param name="level">Baracks level.</param>
+		public void SetToBaracks(string userId, int level)
+		{
+			this.userId = userId;
+			buildingId = FindObjectOfType<Generator> ().GenerateId ();
+			type = buildingType.baracks;
+			this.level = level;
+			if (level == 0) 
+			{
+				health = 20;
+				maxSpace = 0;
+				timeToBuild = 0;
+			}
+			else if (level == 1) 
+			{
+				health = 60;
+				maxSpace = 200;
+				timeToBuild = 10;
+			}
+			else if(level == 2)
+			{
+				health = 70;
+				maxSpace = 300;
+				timeToBuild = 20;
+			}
+			else if(level == 3)
+			{
+				health = 80;
+				maxSpace = 400;
+				timeToBuild = 30;
+			}
+			isSet = true;
+		}
+		#endregion
+		//TODO instantiate camp units
+		public void SetSpawnPoints(int points, double radius, Vector3 center)
+		{
+			double slice = 2 * Math.PI / points;
+			for (int i = 0; i < points; i++)
+			{
+				double angle = slice * i;
+				int newX = (int)(center.x + radius * Math.Cos(angle));
+				int newY = (int)(center.z + radius * Math.Sin(angle));
+				Vector3 p = new Vector3(newX, 0, newY);
+				spawnPoints.Add (p);
+			}
+		}
+		public void SpawnCampUnits()
+		{
+			foreach (Vector3 a in spawnPoints) {
+				GameObject unit = PhotonNetwork.Instantiate ("pref_unit", new Vector3 (a.x, 1.0f, a.z), unitPref.transform.rotation, 0);
+				unit.GetComponent<UnitControl>().userId = userId;
+				unit.GetComponent<UnitControl>().buildingId = buildingId;
+				unit.GetComponent<UnitControl>().unitId = FindObjectOfType<Generator> ().GenerateId ();
+				unit.GetComponent<UnitControl> ().defaultCampfire = gameObject;
+				units.Add (unit);
+			}
+		}
+		public void UpgradeCamp()
+		{
+			if (level < 3) {
+				level++;
+				if (level == 1) 
+				{
+					SetToCamp (userId, 1);
+					textLevel.text = "Level: " + level.ToString ();
+				}
+				else if (level == 2) 
+				{
+					SetToCamp (userId, 2);
+					textLevel.text = "Level: " + level.ToString ();
+				}
+				else if (level == 3) 
+				{
+					SetToCamp (userId, 3);
+					textLevel.text = "Level: " + level.ToString ();
+				}
+			} 
+			else 
+			{
+				Debug.Log ("Reached max level.");
+			}
+		}
+		#region IPunObservable implementation
+
+		public void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+		{
+			if (stream.isWriting) {
+				stream.SendNext (isSet);
+				stream.SendNext (isActive);
+				stream.SendNext (buildingId);
+				stream.SendNext (userId);
+				stream.SendNext (level);
+				stream.SendNext (health);
+			} 
+			else
+			{
+				isSet = (bool)stream.ReceiveNext ();
+				isActive = (bool)stream.ReceiveNext ();
+				buildingId = (string)stream.ReceiveNext ();
+				userId = (string)stream.ReceiveNext ();
+				level = (int)stream.ReceiveNext ();
+				health = (int)stream.ReceiveNext ();
+			}
+		}
+		#endregion
+	}
+}
---
+++ Scripts/BuildingManager.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Networking;
+using System.Runtime.InteropServices;
+
+namespace Com.RegalStick.Oakenland{
+	public class BuildingManager : Photon.PunBehaviour {
+		#region private variables
+		private GameObject campPrefab;
+		private GameObject storagePrefab;
+		private GameObject farmPrefab;
+		private GameObject lumbermillPrefab;
+		private GameObject barracksPrefab;
+		private Plane buildingPlane;
+		private Ray ray;
+		private float distance;
+		private Vector3 position;
+		private string SaveBuildingDataURL = "http://localhost/oakenland/SaveBuildingData.php";
+		#endregion
+		#region public variables
+		public bool isBuilding = false;
+		public List<BuildingControl> campsList;
+		public List<BuildingControl> storagesList;
+		public List<BuildingControl> farmsList;
+		public List<BuildingControl> lumbermillsList;
+		public List<BuildingControl> baracksList;
+		public GameObject newBuilding;
+		#endregion
+
+		void Awake()
+		{
+			campPrefab = (GameObject)Resources.Load ("pref_camp");
+			storagePrefab = (GameObject)Resources.Load ("pref_storage");
+			farmPrefab = (GameObject)Resources.Load ("pref_farm");
+			lumbermillPrefab = (GameObject)Resources.Load ("pref_lumbermill");
+			barracksPrefab = (GameObject)Resources.Load ("pref_lumbermill");
+			buildingPlane = new Plane (Vector3.up, new Vector3 (0.0f, 0.2f, 0.0f));
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		}
+
+		void Update()
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(buildingPlane.Raycast(ray,out distance))
+			{
+				position = ray.GetPoint(distance);	
+				if (newBuilding)
+				{
+					newBuilding.transform.position = position;
+				}
+			}
+			if (isBuilding == true) {
+				if (Input.GetKeyDown (KeyCode.Mouse0) && FindObjectOfType<CheckGroundBelow> ().canBuild == true) 
+				{
+					BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+					if (bc.type.ToString () == "camp") 
+					{
+						campsList.Add (bc);
+						bc.SetSpawnPoints (bc.maxSpace,4,bc.gameObject.transform.position);
+						bc.SpawnCampUnits ();
+					}
+					if (bc.type.ToString () == "storage") 
+					{
+						storagesList.Add (bc);
+					}
+					if (bc.type.ToString () == "farm") 
+					{
+						farmsList.Add (bc);
+					}
+					if (bc.type.ToString () == "lumbermill") 
+					{
+						lumbermillsList.Add (bc);
+					}
+					if (bc.type.ToString () == "baracks") 
+					{
+						baracksList.Add (bc);
+					}
+					newBuilding = null;
+					isBuilding = false;
+				}
+			}
+		}
+
+		#region private methods
+		IEnumerator Save(String buildingId, String tileId, String playerId, Vector3 position)
+		{
+			WWWForm form = new WWWForm();
+			form.AddField ("playerIdPost", playerId);
+			form.AddField ("tileIdPost", tileId);
+			form.AddField ("tilePositionXPost", position.x.ToString ());
+			form.AddField ("tilePositionZPost", position.z.ToString ());
+
+			UnityWebRequest www = UnityWebRequest.Post (SaveBuildingDataURL,form);
+			yield return www.SendWebRequest ();
+
+			if (www.isNetworkError || www.isHttpError) 
+			{
+				Debug.Log (www.error);
+			}
+		}
+		#endregion
+		#region public building methods
+		public void BuildCamp()
+		{
+			FindObjectOfType<CursorControl> ().CursorToCenter ();
+			int money = 100;
+			if(money>=20)
+			{
+				newBuilding = (GameObject)PhotonNetwork.Instantiate("pref_camp",position,campPrefab.transform.rotation,0,null);
+				//TODO Save player camp to the database.
+				BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+				//PhotonView pw = newBuilding.GetComponent<PhotonView> ();
+				bc.SetToCamp (FindObjectOfType<GameManager> ().userId,0);
+				isBuilding = true;
+			}
+		}
+		public void BuildStorage()
+		{
+			FindObjectOfType<CursorControl> ().CursorToCenter ();
+			int money = 100;
+			if(money>=20)
+			{
+				newBuilding = (GameObject)PhotonNetwork.Instantiate("pref_storage",position,storagePrefab.transform.rotation,0,null);
+				//TODO Save player camp to the database.
+				BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+				//PhotonView pw = newBuilding.GetComponent<PhotonView> ();
+				bc.SetToStorage (FindObjectOfType<GameManager> ().userId,0);
+				isBuilding = true;
+			}
+		}
+		public void BuildFarm()
+		{
+			FindObjectOfType<CursorControl> ().CursorToCenter ();
+			int money = 100;
+			if(money>=20)
+			{
+				newBuilding = (GameObject)PhotonNetwork.Instantiate("pref_farm",position,farmPrefab.transform.rotation,0,null);
+				//TODO Save player camp to the database.
+				BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+				//PhotonView pw = newBuilding.GetComponent<PhotonView> ();
+				bc.SetToFarm (FindObjectOfType<GameManager> ().userId,0);
+				isBuilding = true;
+			}
+		}
+		public void BuildLumberMill()
+		{
+			FindObjectOfType<CursorControl> ().CursorToCenter ();
+			int money = 100;
+			if(money>=20)
+			{
+				newBuilding = (GameObject)PhotonNetwork.Instantiate("pref_lumbermill",position,lumbermillPrefab.transform.rotation,0,null);
+				//TODO Save player camp to the database.
+				BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+				//PhotonView pw = newBuilding.GetComponent<PhotonView> ();
+				bc.SetToLumbermill (FindObjectOfType<GameManager> ().userId,0);
+				isBuilding = true;
+			}
+		}
+		public void BuildBaracks()
+		{
+			FindObjectOfType<CursorControl> ().CursorToCenter ();
+			int money = 100;
+			if(money>=20)
+			{
+				newBuilding = (GameObject)PhotonNetwork.Instantiate("pref_baracks",position,barracksPrefab.transform.rotation,0,null);
+				//TODO Save player camp to the database.
+				BuildingControl bc = newBuilding.GetComponent<BuildingControl> ();
+				//PhotonView pw = newBuilding.GetComponent<PhotonView> ();
+				bc.SetToBaracks (FindObjectOfType<GameManager> ().userId,0);
+				isBuilding = true;
+			}
+		}
+		public void UpgradeSelected()
+		{
+			
+		}
+		#endregion
+	}
+}
